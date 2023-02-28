9f7f4e04e72fc8bef5f4b895ea065740bd57d1b4
erabdullah-maker <78157761+erabdullah-maker@users.noreply.github.com>
2023-02-28 15:27:11 +0530

    first commit

---
+++ FundamentalJavaPrograms/src/UnaryOperator.java
@@ -0,0 +1,16 @@
+
+public class UnaryOperator {
+
+	public static void main(String[] args) {
+		
+		int namA=5;
+		int namB=10;
+		int namC=0;
+		
+		System.out.println(namA);
+		System.out.println(namB);
+		System.out.println(namC);
+
+	}
+
+}
